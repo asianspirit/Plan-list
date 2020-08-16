466eed681458a3126421141ea5f19f34d859dc5b
Arslan Shirazhiyev <arslanshirazhiyev@gmail.com>
2020-08-16 16:50:54 +0600

    added styles

---
+++ app/work-js/main.js
@@ -0,0 +1,14 @@
+
+
+
+const add = document.querySelector('.add'),
+    addInput = add.querySelector('.add__input'),
+    confirmBtn = document.querySelector('.add-confirm'),
+    resetBtn = document.querySelector('.add-reset'),
+    planList = document.querySelector('.plan-list'),
+    planListItem = document.querySelector('.plan-list__item'),
+    checkbox = document.querySelector('.checkbox'),
+    checkboxLabel = document.querySelector('.checkbox__label'),
+    trashImg = document.querySelector('.trash__img');
+
+
